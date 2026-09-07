0aabf33c42886b11229a51f8bcdbdf9206744a5c
fcocadena16-lang <fcocadena.16@gmail.com>
2026-09-06 23:26:44 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dc-viewer-shell-v10";
+const CACHE = "dc-viewer-shell-v11";
 const SHELL = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
