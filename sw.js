3773b6f10472d8f902f05b7fa3c50cf265e91ba1
fcocadena16-lang <fcocadena.16@gmail.com>
2026-09-13 00:54:49 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="dc-viewer-shell-v13-3";
-const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE="dc-viewer-shell-v13-4";
+const SHELL=["./","./index.html","./manifest.webmanifest","./apple-touch-icon-v2.png","./icon-192-v2.png","./icon-512-v2.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("message",event=>{if(event.data&&event.data.type==="SKIP_WAITING")self.skipWaiting()});
